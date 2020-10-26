a9f0f137d29c518f1a12a4270ed0109971826bf7
John Poku <jvbpoku@hotmail.com>
2020-10-26 00:41:22 -0700

    cors sucks

--- COMP4537/IndividualAssignment/MemoryGame/js/game-controller.js
+++ COMP4537/IndividualAssignment/MemoryGame/js/game-controller.js
@@ -102,7 +102,6 @@ function fetchRequest(name, userScore)
             {
                 "Accept": "application/json",
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
             },
             body: JSON.stringify(
             {
