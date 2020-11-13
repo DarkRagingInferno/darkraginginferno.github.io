7a41319b93e071ec82379382c83e1fb9f442bbe3
John Poku <jvbpoku@hotmail.com>
2020-11-13 13:41:20 -0800

    trying to access json obj

--- js/lab5/dictionary.js
+++ js/lab5/dictionary.js
@@ -26,36 +26,41 @@ const createEntry = (word, def) =>
     .then(data => console.log(data))
     .catch(err => console.error('Error', err))
     
-    console.log(req)
-    return req
+    return req;
 }
 
 const getWord = async (word) =>
 {
     console.log('Entering GET WORD')
     let foundWord = await fetch(`https://dictionary-jvbp.herokuapp.com/api/definitions?word=${word}`);
-    // let foundWord = fetch(`localhost:3000/api/definitions?word=${word}`);
-    let foundWordText = await foundWord.json()
-    console.log(foundWordText);
+    let foundWordText = await foundWord.json();
+    appendDef(word, foundWordText);
+    
 }
 
 const submit = () =>
 {
-    console.log('Submitting SEARCH WORD')
-   let query =  document.getElementById('search-word').value
-   getWord(query)
+    console.log('Submitting SEARCH WORD');
+   let query =  document.getElementById('search-word').value;
+   getWord(query);
 }
 
 const goToIndex = () => {
     console.log("I should change to index")
 
     window.location.href="https://darkraginginferno.github.io/COMP4537/labs/5/dictionary/index.html"
-    // window.location.href="../../COMP4537/labs/5/dictionary/index.html"
 }
 
 const goToSearch = () => {
     console.log("I should change to search")
     window.location.href="https://darkraginginferno.github.io/COMP4537/labs/5/dictionary/search.html"
-    // window.location.href="../../COMP4537/labs/5/dictionary/search.html"
 }
-// 
+
+const appendDef = (word, jObj) => {
+
+    let termDiv = document.getElementById('term');
+    let defDiv  = document.getElementById('definition');
+
+    termDiv.innerHTML = word;
+    defDiv.innerHTML = jObj[word];
+}
